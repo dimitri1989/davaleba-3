927c4b15e5b5724b554bb048bf3d779fdfe6b899
dimitri kvarelashvili <dimitri.kvarelashvili@gmail.com>
2023-10-04 22:03:42 +0400

    modifed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+    super()
+    this.state = {
+      name : ""
+    }
+  }
+  changeName = () =>{
+    
+      this.setState({
+        names: this.state.name ="name"
+      })
+      return<h3>{(window.alert(this.state.name))}</h3>
+    
+  }
+  render(){
+    return(
+      <React.Fragment>
+        <button type='button' onClick={this.changeName}>დააკლიკე</button>
+      </React.Fragment> 
+    )
+  }
 }
 
 export default App;
